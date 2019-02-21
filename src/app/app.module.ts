83353a6c98036b177201779d754db62e43dbcd2d
Angelus Big <bigcache@hotmail.com>
2019-02-21 00:04:22 -0300

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,22 @@
+import { PagesModule } from './pages/pages.module';
 import { APP_ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    BreadcrumsComponent,
-    SidebarComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
-    BrowserModule, APP_ROUTES
+    BrowserModule, 
+    PagesModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
